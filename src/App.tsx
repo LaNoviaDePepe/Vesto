1a610ab27b64696a0ca2f5232f28ac9e7eafa558
lfernandezf11 <lfernandezf11@iesalbarregas.es>
2026-02-07 14:49:43 +0100

    ✅NEW_Button_component
    Componente Button con variantes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Button from "./components/common/Button"
+
 function App() {
   
 
   return (
     <>
+    <Button variant="primary">Primario</Button>
+    <Button variant="secondary">Secundario</Button>
+    <Button variant="auxiliar">Auxiliar</Button>
+    
         <button className="btn">Button base</button>
         <button className="btn btn-primary">Button primary</button>
         <button className="btn btn-secondary">Button secondary</button>
--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -6,17 +6,16 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export default function Button(
   { children, variant = "primary", className = "", ...props }: ButtonProps) {
-  const baseStyles = "h-[40px] min-w-[90px] flex items-center pl-[8px] pr-[8px] gap-[4px] bg-[#3b49df] text-white rounded-[4px] font-medium text-sm";
-  const hoverStyles = "hover:cursor-pointer";
+  const baseStyles = "btn";
   const variants = {
     primary: "btn-primary",
-    secondary: "bg-gray-200 text-gray-800 hover:bg-gray-300",
-    auxiliar: "bg-gray-200 text-gray-800 hover:bg-gray-300"
+    secondary: "btn-secondary",
+    auxiliar: "btn-auxiliar"
   };
 
     return (
     <button
-      className={`${baseStyles} ${hoverStyles} ${variants[variant]} ${className}`}
+      className={`${baseStyles} ${variants[variant]} ${className}`}
       {...props}
     >
       {children}
