bbc38a3eb4835509a5109dabe846adbd97ca5f73
GabinoMS <jgmuriels01@iesalbarregas.es>
2026-02-09 13:00:43 +0100

    ✅NEW_layouts
    -layouts principales

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import TestingPage from "./pages/TestingPage";
+import NavbarPageFooterLayout from "./layouts/NavbarPageFooterLayout";
+import NavbarPageLayout from "./layouts/NavbarPageLayout";
 
 function App() {
   return (
-    <Router> 
-      {/* Ahora Header y Navbar pueden usar Links porque están dentro del Router */}
-      <TestingPage />
-      
-      
-    </Router>
+
+    <BrowserRouter>
+      <Routes>
+
+        <Route element={<NavbarPageFooterLayout />}>
+
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+
+        </Route>
+
+        <Route element={<NavbarPageLayout />}>
+
+          <Route path="/prendas" element={<PrendasPage />} />
+          <Route path="/addPrenda" element={<AddPrendaPage />} />
+          <Route path="/conjunto" element={<ConjuntoPage />} />
+          <Route path="/addConjunto" element={<AddConjuntoPage />} />
+          <Route path="/perfil" element={<PerfilPage />} />
+
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/layouts/NavbarPageLayout.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/common/Header";
+import { Outlet } from "react-router-dom";
+
+export default function NavbarPageFooterLayout() {
+    return (
+        <div className="min-h-screen flex flex-col">
+
+            <Header />
+
+            <main className="flex-1">
+                <Outlet />
+            </main>
+
+        </div>
+    );
+}
