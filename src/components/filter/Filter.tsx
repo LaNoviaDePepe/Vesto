b2e2a0693e1e9124f89de1c70109083f74bae169
GabinoMS <jgmuriels01@iesalbarregas.es>
2026-02-12 20:02:10 +0100

    ✅NEW_bg_and_filter_fixed
    -background color on all pages
    -filter fixed on all apges

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -2,7 +2,7 @@ import FilterSelect from "./FilterSelect";
 
 export default function Filter() {
     return (
-        <div className="w-full bg-primary-50 flex justify-evenly items-center py-4">
+        <div className="w-full bg-primary-50 flex justify-evenly items-center py-4 fixed top-0 z-50 mt-20">
             <FilterSelect name="filtro" placeholder="Elige un tipo" options={["gorro", "camiseta", "pantalón", "complemento", "calzado"]} />
             <FilterSelect name="filtro" placeholder="Elige un color" options={["negro", "blanco", "rojo", "azul", "verde", "marrón", "amarillo", "gris", "morado"]} />
             <FilterSelect name="filtro" placeholder="Elige una temporada" options={["primavera", "verano", "otoño", "invierno"]} />
