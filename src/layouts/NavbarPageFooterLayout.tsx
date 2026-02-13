0e7e21385689711e3f68d7bf453ef9932cf543f3
lfernandezf11 <lfernandezf11@iesalbarregas.es>
2026-02-13 19:49:41 +0100

    ✅NEW_navbar_change El navbar cambia en función de si el usuario está logueado o no.

--- src/layouts/NavbarPageFooterLayout.tsx
+++ src/layouts/NavbarPageFooterLayout.tsx
@@ -1,19 +1,14 @@
-import { useState } from "react";
 import Footer from "../components/common/Footer";
 import Header from "../components/common/Header";
 import { Outlet } from "react-router-dom";
 
 export default function NavbarPageFooterLayout() {
 
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const toggleLogin = () => {
-        setIsLoggedIn(prev => !prev);
-    };
 
     return (
         <div className="min-h-screen flex flex-col">
 
-            <Header isLoggedIn={isLoggedIn} onToggleLogin={toggleLogin} />
+            <Header />
 
             <main className="flex-1 bg-auxiliary-50">
                 <Outlet />
