e0d7dfcc68286a63d0ab5de94982344cef58f7f2
lfernandezf11 <lfernandezf11@iesalbarregas.es>
2026-03-11 12:18:02 +0100

    🔥FIX_ El usuario admin se redirige al dashboard al hacer login (había que modificar PublicRoute).

--- src/router/PublicRoute.tsx
+++ src/router/PublicRoute.tsx
@@ -5,8 +5,13 @@ import { useAuthStore } from "../stores/authStore";
 function PublicRoute() {
 	// Obtenemos el valor de isAuthenticated 
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const isAdmin = useAuthStore((state) => state.isAdmin);
+
 
   if (isAuthenticated) {
+    if(isAdmin) {
+      return <Navigate to="/admin/dashboard" replace />;
+    }
     // Si está autenticado, redirige al armario
     return <Navigate to="/closet" replace />;
   }
