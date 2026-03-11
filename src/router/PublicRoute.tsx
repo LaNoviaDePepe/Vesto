997acfd44804666ff65b9a3ad6cbd4897aca362c
lfernandezf11 <lfernandezf11@iesalbarregas.es>
2026-03-11 16:35:27 +0100

    🔄 UPDATE Formulario de crear conjunto componetizado

--- src/router/PublicRoute.tsx
+++ src/router/PublicRoute.tsx
@@ -1,22 +1,16 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuthStore } from "../stores/authStore";
 
-
 function PublicRoute() {
-	// Obtenemos el valor de isAuthenticated 
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
   const isAdmin = useAuthStore((state) => state.isAdmin);
-
 
   if (isAuthenticated) {
     if(isAdmin) {
       return <Navigate to="/admin/dashboard" replace />;
     }
-    // Si está autenticado, redirige al armario
     return <Navigate to="/closet" replace />;
   }
-
-  // Si está autenticado, renderiza el contenido protegido
   return <Outlet />;
 }
 
